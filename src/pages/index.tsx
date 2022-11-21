c971930cc871b750fa75cb630384563c0c3705a2
Kevin Hoerr <kjhoerr@protonmail.com>
2022-11-21 15:46:03 -0500

    Remove unused files; consistent icon styling (#11)
    * Remove unused files; consistent icon styling
    
    * Update GitHub actions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import BlockLink from "../components/BlockLink";
 import Footer from "../components/Footer";
 import { BlockBody, BlockHeader, Content } from "../styles";
 import package_json from "../../package.json";
+import { IconContext } from "react-icons";
 
 const VERSION = package_json.version;
 
@@ -13,58 +14,61 @@ const VERSION = package_json.version;
 const IndexPage = (): React.ReactElement => {
   return (
     <main>
-      <Content>
-        <div>
-          <StaticImage
-            src="../images/main.png"
-            alt="Picture of Kevin Hoerr"
-            placeholder="tracedSVG"
-            layout="fixed"
-            width={350}
-            height={350}
-          />
-        </div>
-        <BlockHeader>Hello!</BlockHeader>
-        <BlockBody>
-          <p>
-            I&apos;m a computer science and math graduate from Millersville
-            University. I work as an IT consultant and specialize in development
-            operations and systems validation for web applications.
-          </p>
-          <p>
-            My most recent projects have been focused on full-stack development.
-            I use Kubernetes for orchestration and NextJS for the front-end. For
-            back-end development, I have built services enabled by Java with
-            Spring Boot, Quarkus, and rust+actix-web with a GraphQL serving
-            public API.
-          </p>
-          <p>
-            I run a personal Kubernetes cluster at home using MicroOS and k3d. I
-            have also run multiple clusters via DigitalOcean with deployments
-            dedicated for hosting git projects, CI/CD, and code coverage. Since
-            then all projects have been moved to GitHub.
-          </p>
-          <em>- Kevin H.</em>
-        </BlockBody>
-        <BlockLink
-          href="https://cybr.es/@kjhoerr"
-          rel="me"
-          aria-label="My Mastodon account"
-        >
-          <FaMastodon size={20} style={{ marginBottom: "-4px" }} />{" "}
-          @kjhoerr@cybr.es
-        </BlockLink>
-        <BlockLink
-          href="https://github.com/kjhoerr"
-          rel="me"
-          aria-label="My GitHub account with my most active personal projects"
-        >
-          <FaGithub size={20} style={{ marginBottom: "-4px" }} /> kjhoerr on
-          GitHub
-        </BlockLink>
-      </Content>
+      <IconContext.Provider
+        value={{ size: "20", style: { marginBottom: "-4px" } }}
+      >
+        <Content>
+          <div>
+            <StaticImage
+              src="../images/main.png"
+              alt="Picture of Kevin Hoerr"
+              placeholder="tracedSVG"
+              layout="fixed"
+              width={350}
+              height={350}
+            />
+          </div>
+          <BlockHeader>Hello!</BlockHeader>
+          <BlockBody>
+            <p>
+              I&apos;m a computer science and math graduate from Millersville
+              University. I work as an IT consultant and specialize in
+              development operations and systems validation for web
+              applications.
+            </p>
+            <p>
+              My most recent projects have been focused on full-stack
+              development. I use Kubernetes for orchestration and NextJS for the
+              front-end. For back-end development, I have built services enabled
+              by Java with Spring Boot, Quarkus, and rust+actix-web with a
+              GraphQL serving public API.
+            </p>
+            <p>
+              I run a personal Kubernetes cluster at home using MicroOS and k3d.
+              I have also run multiple clusters via DigitalOcean with
+              deployments dedicated for hosting git projects, CI/CD, and code
+              coverage. Since then all projects have been moved to GitHub.
+            </p>
+            <em>- Kevin H.</em>
+          </BlockBody>
+          <BlockLink
+            href="https://cybr.es/@kjhoerr"
+            rel="me"
+            aria-label="My Mastodon account"
+          >
+            <FaMastodon /> @kjhoerr@cybr.es
+          </BlockLink>
+          <BlockLink
+            href="https://github.com/kjhoerr"
+            rel="me"
+            aria-label="My GitHub account with my most active personal projects"
+          >
+            <FaGithub /> kjhoerr on GitHub
+          </BlockLink>
+        </Content>
 
-      <Footer timestamp={VERSION} />
+        <Footer timestamp={VERSION} />
+      </IconContext.Provider>
     </main>
   );
 };
