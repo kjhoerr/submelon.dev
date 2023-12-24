f580eb6879fbb97efbf90a399402db41572a2d2f
Kevin J Hoerr <kjhoerr@protonmail.com>
2023-12-23 23:50:08 -0500

    Re-add typescript for eslint

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { getTimestamp } from "./util/timestamp";
 import BuildInfo from "../config.json";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function (eleventyConfig: any) {
   eleventyConfig.addPassthroughCopy({
     "./src/static/": "/",
